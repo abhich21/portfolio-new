a29842cf464a4e294222cae69a1034fc819f4a97
Abhishek Choudhary <96258754+abhich21@users.noreply.github.com>
2025-11-26 16:40:46 +0530

    updated portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -191,6 +191,10 @@ if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+} else {
+    // Default to dark theme
+    document.body.classList.add(darkTheme)
+    themeButton.classList.add(iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
